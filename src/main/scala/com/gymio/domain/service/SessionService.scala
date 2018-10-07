fb4c8683055cb1ef8e63555a140bfc192e07998d
Truong Hoang <truonghoang9011@gmail.com>
2018-10-07 12:45:04 +0200

    Minor

--- src/main/scala/com/gymio/domain/service/SessionService.scala
+++ src/main/scala/com/gymio/domain/service/SessionService.scala
@@ -37,17 +37,7 @@ object SessionService {
         .map(w => w.value * scheme.weightPercentage)
     } yield res
 
-    /* Alternative way to do it below. The above thing is called "for comprehension"
-     * and is basically just a series of flatMaps. flatMap is a combination of
-     * .map and .flatten. .map is a higher order function that operates on all
-     * values in the monad rather than the monad itself.
-     * For example, List is a monad. So for a List[Int], .map will operate
-     * on the values that are of type Int.
-     * List(1, 2, 3, 4, 5).map(intValue => intValue * 2) will yield:
-     * List(2, 4, 6, 8, 10) where intValue => intValue * 2 is just
-     * an anonymous function that takes an intValue and returns the product
-     */
-
+//    Alternative way to do it below.
 //    setSchemesByWeek.get(s.week)
 //      .flatMap(schemes => schemes.get(s.step._1)
 //        .flatMap(scheme => exercisesByDay.get(s.day).map(exs => exs.apply(s.step._2))
