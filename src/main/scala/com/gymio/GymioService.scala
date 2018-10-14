f5bad5493044bd6589fd458ed1d7b17ebf951215
fulder <misad90@gmail.com>
2018-10-14 12:51:34 +0200

    Add session inmem db

--- src/main/scala/com/gymio/GymioService.scala
+++ src/main/scala/com/gymio/GymioService.scala
@@ -13,8 +13,9 @@ import org.http4s.dsl.io._
 import org.http4s.{HttpRoutes, Request, Response}
 
 class GymioService {
-  var log: Map[UUID, ExerciseLog] = Map()
-  var eventStore: Seq[Event]      = List()
+  var log: Map[UUID, ExerciseLog]           = Map()
+  var eventStore: Seq[Event]                = List()
+  var sessionStore: Map[UUID, Seq[Session]] = Map()
 
   val gymioService: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes
     .of[IO] {
@@ -22,11 +23,13 @@ class GymioService {
         Ok(log.asJson)
       case req @ POST -> Root / "log" / UUIDVar(userId) / "add" =>
         logExerciseForUser(req, userId)
+      case req @ POST -> Root / "session" / UUIDVar(userId) / "complete" =>
+        completeSession(req, userId)
     }
     .orNotFound
 
   def logExerciseForUser(req: Request[IO], userId: UUID): IO[Response[IO]] = {
-    val userLog = log.get(userId).getOrElse(ExerciseLog(List()))
+    val userLog = log.getOrElse(userId, ExerciseLog(List()))
 
     for {
       c   <- req.as[Command]
@@ -46,6 +49,20 @@ class GymioService {
   def updateStore(event: Event): IO[Seq[Event]] = {
     eventStore = eventStore :+ event
     IO(eventStore)
+  }
+
+  def completeSession(req: Request[IO], userId: UUID): IO[Response[IO]] = {
+    for {
+      s   <- req.as[Session]
+      _   <- updateSessionStore(userId, s)
+      res <- Ok(sessionStore.asJson)
+    } yield res
+  }
+
+  def updateSessionStore(userId: UUID, session: Session): IO[Map[UUID, Seq[Session]]] = {
+    val sessionList = sessionStore.getOrElse(userId, List()) :+ session
+    sessionStore += userId -> sessionList
+    IO(sessionStore)
   }
 
 }
--- src/main/scala/com/gymio/domain/model/Session.scala
+++ src/main/scala/com/gymio/domain/model/Session.scala
@@ -1,5 +1,14 @@
 package com.gymio.domain.model
 
-import io.circe.generic.auto._
+import java.util.UUID
 
-case class Session(day: Int, week: Int, step: (Int, Int))
+import cats.effect.IO
+import io.circe.generic.auto._
+import org.http4s.EntityDecoder
+import org.http4s.circe.jsonOf
+
+case class Session(userId: UUID, day: Int, week: Int, step: (Int, Int))
+
+object Session {
+  implicit val decoder: EntityDecoder[IO, Session] = jsonOf[IO, Session]
+}
