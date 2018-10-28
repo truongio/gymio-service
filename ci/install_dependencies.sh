49a06e6a16dcab54f8d60cae8443f7feefa4620c
fulder <misad90@gmail.com>
2018-10-28 14:04:54 +0100

    Revert to normal installation

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-gcloud components install kubectl
-
 # Install kubectl
-#sudo apt-get update && sudo apt-get install -y apt-transport-https
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-#sudo apt-get update
-#sudo apt-get install -y kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 # Update gcloud
-#sudo apt-get --only-upgrade -y install kubectl google-cloud-sdk
+sudo apt-get --only-upgrade -y install kubectl google-cloud-sdk
