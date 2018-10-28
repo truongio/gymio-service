ca37d33329cb49aadb9833f83ad314d5d2116a06
fulder <misad90@gmail.com>
2018-10-28 16:50:02 +0100

    Fix image

--- ci/deploy_k8s.sh
+++ ci/deploy_k8s.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo -e "\e[31mThree arguments expected. Usage: $0 <GCLOUD_SERVICE_ACOUNT_JSON_FILE> <DB_USER> <DB_PASSWORD>\e[39m"
-    echo -e "\e[31mExample: $0 service_account.json root pass\e[39m"
+if [ $# -ne 5 ]; then
+    echo -e "\e[31mThree arguments expected. Usage: $0 <GCLOUD_SERVICE_ACOUNT_JSON_FILE> <DB_USER> <DB_PASSWORD> <BRANCH> <SHA>\e[39m"
+    echo -e "\e[31mExample: $0 service_account.json root pass develop 123123\e[39m"
     exit 1
 fi
 
 DB_SERVICE_ACCOUNT_FILE=$1
 GYMIO_DB_USER=$2
 GYMIO_DB_PASSWORD=$3
+REPOSITORY="gcr.io/gymio-220023/gymio-service"
+BRANCH=$4
+SHA=$5
 
 # Provision
+GYMIO_SERVICE_IMAGE=${REPOSITORY}:${BRANCH}-${SHA}
 envsubst < deployment/gymio-deployment.yaml > deployment/gymio-deployment-rendered.yaml
 
 # Create secrets
