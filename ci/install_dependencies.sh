1323c27e8eda1f5fd938a5ac3d4657df14cf9e10
fulder <misad90@gmail.com>
2018-10-28 14:35:12 +0100

    Install gcloud after apt-get

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -2,6 +2,13 @@
 
 gcloud version
 
+# Install kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]
 then
   rm -rf $HOME/google-cloud-sdk
@@ -9,15 +16,5 @@ then
   curl https://sdk.cloud.google.com | bash
 fi
 
-# Add gcloud to $PATH
-PATH=$PATH:${HOME}/google-cloud-sdk/bin
-source $HOME/google-cloud-sdk/path.bash.inc
-
-# Install kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
 
 gcloud version
