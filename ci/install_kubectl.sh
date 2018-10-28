851825f75a77d7d465d45257511de93abdfb4b1f
fulder <misad90@gmail.com>
2018-10-28 13:53:04 +0100

    Update kubectl and gcloud

--- ci/install_kubectl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-mkdir -p .bin; mv ./kubectl .bin/kubectl && chmod +x .bin/kubectl
