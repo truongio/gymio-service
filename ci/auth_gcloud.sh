bbe2bb8ecdb0f09b9e3ac4afed735fce77ef6b03
fulder <misad90@gmail.com>
2018-10-28 15:12:36 +0100

    Cleanup

--- ci/auth_gcloud.sh
+++ ci/auth_gcloud.sh
@@ -14,5 +14,3 @@ gcloud auth activate-service-account --key-file=${SERVICE_ACCOUNT_FILE}
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud container clusters get-credentials gymio-cluster
-
-kubectl get pods
