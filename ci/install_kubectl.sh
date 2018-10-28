30eeeba7592cbb8dafd18eec690528514df7cf2a
fulder <misad90@gmail.com>
2018-10-28 13:49:39 +0100

    Change kubectl installation to curl

--- ci/install_kubectl.sh
+++ ci/install_kubectl.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+mkdir -p .bin; mv ./kubectl .bin/kubectl && chmod +x .bin/kubectl
