83c3a3d50dc391448824463a33947e3a7a0f7109
fulder <misad90@gmail.com>
2018-10-28 15:06:25 +0100

    Remove incorrect cache

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -3,6 +3,7 @@
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]
 then
   sudo rm -rf /usr/lib/google-cloud-sdk
+  sudo rm -rf $HOME/google-cloud-sdk
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
   curl https://sdk.cloud.google.com | bash
 fi
