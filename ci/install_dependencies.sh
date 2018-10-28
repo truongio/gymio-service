434fa1097e3d2c46924e63da11f9ccd9d386232c
fulder <misad90@gmail.com>
2018-10-28 15:06:50 +0100

    Add comments

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -2,7 +2,9 @@
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]
 then
+  # Remove old travis SDK
   sudo rm -rf /usr/lib/google-cloud-sdk
+  # Remove old/wrong cache
   sudo rm -rf $HOME/google-cloud-sdk
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
   curl https://sdk.cloud.google.com | bash
