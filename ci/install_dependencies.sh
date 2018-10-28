f26c89a94534c69e71e8b8168d88885f1ea08729
fulder <misad90@gmail.com>
2018-10-28 15:25:41 +0100

    Update kubectl oinly if cache doesn't exist

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -10,6 +10,5 @@ then
   sudo rm -rf $HOME/google-cloud-sdk
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
   curl https://sdk.cloud.google.com | bash
+  gcloud --quiet components update kubectl
 fi
-
-gcloud --quiet components update kubectl
