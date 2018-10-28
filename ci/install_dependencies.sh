c2956149e5fcbfe0d9f63db531771ce0c96ee093
fulder <misad90@gmail.com>
2018-10-28 14:26:14 +0100

    Don't update gcloud twice

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -6,11 +6,11 @@ if [ ! -d "$HOME/google-cloud-sdk/bin" ]
 then
   rm -rf $HOME/google-cloud-sdk
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-  curl https://sdk.cloud.google.com | bash;
+  curl https://sdk.cloud.google.com | bash
 fi
 
 # Add gcloud to $PATH
-source /home/travis/google-cloud-sdk/path.bash.inc
+source $HOME/google-cloud-sdk/path.bash.inc
 
 gcloud version
 
@@ -20,6 +20,3 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-
-# Update gcloud
-sudo apt-get --only-upgrade -y install kubectl google-cloud-sdk
