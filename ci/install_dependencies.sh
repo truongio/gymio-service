278c5652a14ccb2278b0dbc6490e4caae065b05c
fulder <misad90@gmail.com>
2018-10-28 14:17:22 +0100

    Update gcloud

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+gcloud version
+
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]
+then
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+  curl https://sdk.cloud.google.com | bash;
+fi
+
+# Add gcloud to $PATH
+source /home/travis/google-cloud-sdk/path.bash.inc
+
 # Install kubectl
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
